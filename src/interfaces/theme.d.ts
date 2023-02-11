781a5d993007257d47275732b11f3c608b95bedc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-11 19:10:58 +0530

    Update theme.d.ts

--- src/interfaces/theme.d.ts
+++ src/interfaces/theme.d.ts
@@ -1,15 +1,18 @@
+/*eslint-disable */
 import "@pankod/refine-mui";
-
 export interface CustomTheme {
-  // Add custom variables here like below:
-  // status: {
-  //   danger: string;
-  // };
+    // Add custom variables here like below:
+    // status: {
+    //   danger: string;
+    // };
 }
 
 declare module "@pankod/refine-mui" {
-  interface Theme extends import("@pankod/refine-mui").Theme, CustomTheme {}
-  interface ThemeOptions
-    extends import("@pankod/refine-mui").ThemeOptions,
-      CustomTheme {}
+    // @ts-ignore
+    interface Theme extends import("@pankod/refine-mui").Theme, CustomTheme {}
+    interface ThemeOptions
+        // @ts-ignore
+        extends import("@pankod/refine-mui").ThemeOptions,
+            CustomTheme {}
 }
+/*eslint-enable */
