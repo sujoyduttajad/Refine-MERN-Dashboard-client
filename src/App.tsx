a1b8b7aef7845692b6d3235157e04d7869e4bf73
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-11 12:36:34 +0530

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+import { Refine, AuthProvider } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  RefineSnackbarProvider,
+  CssBaseline,
+  GlobalStyles,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-mui";
+
+import dataProvider from "@pankod/refine-simple-rest";
+import { MuiInferencer } from "@pankod/refine-inferencer/mui";
+import routerProvider from "@pankod/refine-react-router-v6";
+import axios, { AxiosRequestConfig } from "axios";
+import { ColorModeContextProvider } from "contexts";
+import { Title, Sider, Layout, Header } from "components/layout";
+import { Login } from "pages/login";
+import { CredentialResponse } from "interfaces/google";
+import { parseJwt } from "utils/parse-jwt";
+
+const axiosInstance = axios.create();
+axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
+  const token = localStorage.getItem("token");
+  if (request.headers) {
+    request.headers["Authorization"] = `Bearer ${token}`;
+  } else {
+    request.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  return request;
+});
+
+function App() {
+  const authProvider: AuthProvider = {
+    login: ({ credential }: CredentialResponse) => {
+      const profileObj = credential ? parseJwt(credential) : null;
+
+      if (profileObj) {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            ...profileObj,
+            avatar: profileObj.picture,
+          })
+        );
+      }
+
+      localStorage.setItem("token", `${credential}`);
+
+      return Promise.resolve();
+    },
+    logout: () => {
+      const token = localStorage.getItem("token");
+
+      if (token && typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        axios.defaults.headers.common = {};
+        window.google?.accounts.id.revoke(token, () => {
+          return Promise.resolve();
+        });
+      }
+
+      return Promise.resolve();
+    },
+    checkError: () => Promise.resolve(),
+    checkAuth: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        return Promise.resolve();
+      }
+      return Promise.reject();
+    },
+
+    getPermissions: () => Promise.resolve(),
+    getUserIdentity: async () => {
+      const user = localStorage.getItem("user");
+      if (user) {
+        return Promise.resolve(JSON.parse(user));
+      }
+    },
+  };
+
+  return (
+    <ColorModeContextProvider>
+      <CssBaseline />
+      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+      <RefineSnackbarProvider>
+        <Refine
+          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+          notificationProvider={notificationProvider}
+          ReadyPage={ReadyPage}
+          catchAll={<ErrorComponent />}
+          resources={[
+            {
+              name: "posts",
+              list: MuiInferencer,
+              edit: MuiInferencer,
+              show: MuiInferencer,
+              create: MuiInferencer,
+              canDelete: true,
+            },
+          ]}
+          Title={Title}
+          Sider={Sider}
+          Layout={Layout}
+          Header={Header}
+          routerProvider={routerProvider}
+          authProvider={authProvider}
+          LoginPage={Login}
+        />
+      </RefineSnackbarProvider>
+    </ColorModeContextProvider>
+  );
+}
+
+export default App;
