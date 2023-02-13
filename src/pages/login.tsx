032f37a92462fa0b8840088ed02c5d76c74000f8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-13 13:48:07 +0530

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -20,7 +20,8 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: "your-client-id",
+          // put your own client_id
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
