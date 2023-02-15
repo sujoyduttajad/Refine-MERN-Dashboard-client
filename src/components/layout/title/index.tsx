61d3249fcc764b530330de0ef593ecaabe9b4979
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-15 14:21:19 +0530

    Update index.tsx

--- src/components/layout/title/index.tsx
+++ src/components/layout/title/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRouterContext, TitleProps } from "@pankod/refine-core";
 import { Button } from "@pankod/refine-mui";
+import { logo, yariga } from "assets";
 
 export const Title: React.FC<TitleProps> = ({ collapsed }) => {
   const { Link } = useRouterContext();
@@ -9,9 +10,9 @@ export const Title: React.FC<TitleProps> = ({ collapsed }) => {
     <Button fullWidth variant="text" disableRipple>
       <Link to="/">
         {collapsed ? (
-          <img src="/refine-collapsed.svg" alt="Refine" width="28px" />
+          <img src={logo} alt="Yariga" width="28px" />
         ) : (
-          <img src="/refine.svg" alt="Refine" width="140px" />
+          <img src={yariga} alt="Yariga" width="140px" />
         )}
       </Link>
     </Button>
