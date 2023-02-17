0f03b49f039c3dc4251103d4bda64c6aa33f0c88
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-17 14:26:41 +0530

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,26 @@
-import { useList } from "@pankod/refine-core/dist/hooks"
+import { useList } from "@pankod/refine-core/dist/hooks";
 // import { useList } from "@pankod/refine-core"
+import {
+  PieChart,
+  PropertyCard,
+  PropertyReferrals,
+  TotalRevenue,
+  TopAgent,
+} from "components";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
 
 // Dashboard page
 const Home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart />
+      </Box>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
