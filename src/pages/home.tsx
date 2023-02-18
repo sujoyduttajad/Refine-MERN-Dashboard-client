c8c01e08f9348b0ede3635710b5596fd3958ab3b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-18 17:28:30 +0530

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,7 +17,30 @@ const Home = () => {
         Dashboard
       </Typography>
       <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
-        <PieChart />
+        <PieChart
+          title="Properties for Sale"
+          value={684}
+          color={["#475be8", "#e4e8ef"]}
+          series={[75, 25]}
+        />
+         <PieChart
+          title="Properties for Rent"
+          value={550}
+          color={["#18C784", "#e4b8ef"]}
+          series={[60, 40]}
+        />
+         <PieChart
+          title="Total Customers"
+          value={5684}
+          color={["#FF7B00", "#F3EBCF"]}
+          series={[68, 32]}
+        />
+         <PieChart
+          title="Properties for Cities"
+          value={445}
+          color={["#FF3A3A", "#E9D8E0"]}
+          series={[88, 22]}
+        />
       </Box>
     </Box>
   );
