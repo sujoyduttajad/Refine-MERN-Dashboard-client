5d89b49ada83134f42b32a48eae0226d92f1fad1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-18 18:42:09 +0530

    Update PieChart.tsx

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,12 +1,19 @@
 import { Typography, Box, Stack } from "@pankod/refine-mui";
 import { PieChartProps } from "interfaces/home";
 
-const PieChart = ({ title, value, series, colors}: PieChartProps) => {
+const PieChart = ({ title, value, series, colors }: PieChartProps) => {
   return (
     <Box>
-
+      <Stack direction={"column"}>
+        <Typography fontSize={14} color="#808191">
+          {title}
+        </Typography>
+        <Typography fontSize={24} color="#11142d" fontWeight={700} mt={1}>
+          {value}
+        </Typography>
+      </Stack>
     </Box>
-  )
-}
+  );
+};
 
-export default PieChart
+export default PieChart;
