65ccd259a92c8a19421c5c6031064a80f34589ef
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-18 17:28:46 +0530

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -20,25 +20,25 @@ const Home = () => {
         <PieChart
           title="Properties for Sale"
           value={684}
-          color={["#475be8", "#e4e8ef"]}
+          colors={["#475be8", "#e4e8ef"]}
           series={[75, 25]}
         />
          <PieChart
           title="Properties for Rent"
           value={550}
-          color={["#18C784", "#e4b8ef"]}
+          colors={["#18C784", "#e4b8ef"]}
           series={[60, 40]}
         />
          <PieChart
           title="Total Customers"
           value={5684}
-          color={["#FF7B00", "#F3EBCF"]}
+          colors={["#FF7B00", "#F3EBCF"]}
           series={[68, 32]}
         />
          <PieChart
           title="Properties for Cities"
           value={445}
-          color={["#FF3A3A", "#E9D8E0"]}
+          colors={["#FF3A3A", "#E9D8E0"]}
           series={[88, 22]}
         />
       </Box>
