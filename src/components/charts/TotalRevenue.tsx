ffd7e5a56147150cfbfc8cf561a4fd7329ce181d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-19 12:29:32 +0530

    Update TotalRevenue.tsx

--- src/components/charts/TotalRevenue.tsx
+++ src/components/charts/TotalRevenue.tsx
@@ -1,11 +1,21 @@
 import { Box, Typography, Stack } from "@pankod/refine-mui";
 import ReactApexChart from "react-apexcharts";
 import { ArrowCircleUpRounded } from "@mui/icons-material";
-
+import { TotalRevenueOptions, TotalRevenueSeries } from "./chart.config";
 
 const TotalRevenue = () => {
   return (
-    <div>TotalRevenue</div>
+    <Box
+      p={4}
+      flex={1}
+      bgcolor="#fcfcfc"
+      id="chart"
+      display="flex"
+      flexDirection="column"
+      borderRadius="15px"
+    >
+
+    </Box>
   )
 }
 
