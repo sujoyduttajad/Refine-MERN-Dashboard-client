226ba6f4e87947e7667862f341e7f3499bab20f8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-19 20:32:05 +0530

    Update PropertyReferrals.tsx

--- src/components/charts/PropertyReferrals.tsx
+++ src/components/charts/PropertyReferrals.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Box, Typography, Stack } from "@pankod/refine-mui";
+import { propertyReferralsInfo } from "constants";
 
 const PropertyReferrals = () => {
   return (
-    <div>PropertyReferrals</div>
-  )
-}
+    <Box
+      p={4}
+      bgcolor="#fcfcfc"
+      id="chart"
+      minWidth={490}
+      display="flex"
+      flexDirection="column"
+      borderRadius="15px"
+    >
+      <Typography fontSize={18} fontWeight={600} color="#11142d">
+        Property Referrals
+      </Typography>
+    </Box>
+  );
+};
 
-export default PropertyReferrals
+export default PropertyReferrals;
