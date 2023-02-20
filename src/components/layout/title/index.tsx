f77d6109f895bdda730a713c87cc9f17fca43712
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-20 14:44:12 +0530

    Update index.tsx

--- src/components/layout/title/index.tsx
+++ src/components/layout/title/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouterContext, TitleProps } from "@pankod/refine-core";
 import { Button } from "@pankod/refine-mui";
-import { logo, yariga } from "assets";
+import { logo, evoiaLight } from "assets";
 
 export const Title: React.FC<TitleProps> = ({ collapsed }) => {
   const { Link } = useRouterContext();
@@ -12,7 +12,7 @@ export const Title: React.FC<TitleProps> = ({ collapsed }) => {
         {collapsed ? (
           <img src={logo} alt="Yariga" width="28px" />
         ) : (
-          <img src={yariga} alt="Yariga" width="140px" />
+          <img src={evoiaLight} alt="Yariga" width="140px" />
         )}
       </Link>
     </Button>
