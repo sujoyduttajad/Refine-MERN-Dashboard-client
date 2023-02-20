cdb1ce4c56f52f5cd881ed26649a3d8c59c11a17
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-20 16:52:21 +0530

    Update all-properties.tsx

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -2,12 +2,16 @@ import { Add } from "@mui/icons-material";
 import { useList } from "@pankod/refine-core";
 import { Box, Typography, Stack } from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
-
+import { PropertyCard, CustomButton } from "components";
 
 const AllProperties = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>AllProperties</div>
+    <Box>
+      <Stack></Stack>
+    </Box>
   )
-}
+} 
 
 export default AllProperties
