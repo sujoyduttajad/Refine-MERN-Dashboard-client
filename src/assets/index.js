1cbb74e44944369f16db1496049dc1cdcf1bdb98
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-20 14:44:14 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import yariga from './yariga.svg';
+import evoiaLight from './evoiaLight.svg';
+import evoiaDark from './evoiaDark.svg';
 
 export {
   logo,
   yariga,
+  evoiaLight,
+  evoiaDark
 };
