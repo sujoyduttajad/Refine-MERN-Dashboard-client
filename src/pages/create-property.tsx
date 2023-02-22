d9f18a59f06dc9fd3eccb43de3ea953222008d8f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-22 11:37:53 +0530

    Update create-property.tsx

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import { useGetIdentity } from '@pankod/refine-core'
-import { FieldValues, useForm } from '@pankod/refine-react-hook-form'
-import { useNavigate } from '@pankod/refine-react-router-v6'
-import Form from 'components/common/Form'
+import { useState } from "react";
+import { useGetIdentity } from "@pankod/refine-core";
+import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
+import { useNavigate } from "@pankod/refine-react-router-v6";
+import Form from "components/common/Form";
 
 const CreateProperty = () => {
   const navigate = useNavigate();
   const { data: user } = useGetIdentity();
 
-  const [propertyImage, setPropertyImage] = useState();
-  
-  return (
-    <div>Create Property</div>
-  )
-}
+  const [propertyImage, setPropertyImage] = useState({ name: "", url: "" });
+  const {
+    refineCore: { onFinish, formLoading },
+    register,
+    handleSubmit,
+  } = useForm();
 
-export default CreateProperty
+  return <div>Create Property</div>;
+};
+
+export default CreateProperty;
