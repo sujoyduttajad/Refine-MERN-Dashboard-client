fef9e76cb0df063d415f2be8ff1d3ec17f9e0872
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-23 13:22:44 +0530

    Update Form.tsx

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -8,7 +8,7 @@ import {
   Stack,
   Select,
   MenuItem,
-  Button
+  Button,
 } from "@pankod/refine-mui";
 
 import { FormProps } from "interfaces/common";
@@ -29,16 +29,20 @@ const Form = ({
       <Typography fontSize={25} fontWeight={700} color="#11142d">
         {type} a Property
       </Typography>
-      <Box 
-        mt={2.5} 
-        borderRadius="15px" 
-        padding="20px" 
-        bgcolor="#fafafa"
-      >
-
+      <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#fafafa">
+        <form
+          style={{
+            marginTop: "20px",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+          onSubmit={handleSubmit(onFinishHandler)}
+        ></form>
       </Box>
     </Box>
-  )
+  );
 };
 
 export default Form;
