5a6b26a629cb3bfa629a46f2c4edddc5b292ae1d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-24 17:20:05 +0530

    Update Form.tsx

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -183,10 +183,42 @@ const Form = ({
 
           <Stack direction="column" gap={1} justifyContent="center" mb={2}>
             <Stack direction="row" gap={2}>
-              <Typography color="#11142d" fontSize={16} fontWeight={500} my="10px">
+              <Typography
+                color="#11142d"
+                fontSize={16}
+                fontWeight={500}
+                my="10px"
+              >
                 Property Photo
               </Typography>
+              <Button
+                component="label"
+                sx={{
+                  width: "fit-content",
+                  color: "#2ed480",
+                  textTransform: "capitalize",
+                  fontSize: 16,
+                }}
+              >
+                Upload *
+                <input
+                  hidden
+                  accept="image/*"
+                  type="file"
+                  onChange={(e) => {
+                    // @ts-ignore
+                    handleImageChange(e.target.files[0]);
+                  }}
+                />
+              </Button>
             </Stack>
+            <Typography
+              fontSize={14}
+              color="#808191"
+              sx={{ wordBreak: "break-all" }}
+            >
+              {propertyImage?.name}
+            </Typography>
           </Stack>
         </form>
       </Box>
