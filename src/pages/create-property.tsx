957d111e5fd55e2733f71b7a9fa8ccc248ea9cd3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-02-25 13:26:17 +0530

    Update create-property.tsx

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -15,8 +15,21 @@ const CreateProperty = () => {
     handleSubmit,
   } = useForm();
 
-  const handleImageChange = () => {}
-  const onFinishHandler = () => {}
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) => new Promise<string>((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile);
+    });
+
+    reader(file).then((result: string) => setPropertyImage({ name: file?.name, url: result }));
+  };
+  
+  const onFinishHandler = async (data: FieldValues) => {
+    if(!propertyImage.name) return alert('Please select an image');
+    
+    await onFinish({ ...data, photo: propertyImage.url, email: user.email })
+  };
 
   // Rendering a reusable form
   return (
