e98108fe98471a41dc62ad9f6370eca9a913ce6b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-16 14:32:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -147,7 +147,7 @@ function App() {
       <ThemeProvider theme={customTheme}>
         <RefineSnackbarProvider>
           <Refine
-            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            dataProvider={dataProvider("http://localhost:8080/api/v1")}
             notificationProvider={notificationProvider}
             ReadyPage={ReadyPage}
             catchAll={<ErrorComponent />}
