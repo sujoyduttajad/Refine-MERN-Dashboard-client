8be7b514cc26a5f86eea0dba19da5e8d0fe55e91
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-18 18:49:44 +0530

    Update all-properties.tsx

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,5 +1,5 @@
 import { Add } from "@mui/icons-material";
-// import { useList } from "@pankod/refine-core";
+import { useTable } from "@pankod/refine-core";
 import { Box, Typography, Stack } from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
 import { PropertyCard, CustomButton } from "components";
@@ -7,28 +7,26 @@ import { PropertyCard, CustomButton } from "components";
 const AllProperties = () => {
   const navigate = useNavigate();
 
+  const {
+    tableQueryResult: { data, isLoading, isError },
+  } = useTable();
+
   return (
     <Box>
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-      >
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Typography fontSize={25} fontWeight={700} color="#11142d">
           All Properties
         </Typography>
-        <CustomButton 
+        <CustomButton
           title="Add Property"
-          handleClick={() => navigate('/properties/create')}
+          handleClick={() => navigate("/properties/create")}
           backgroundColor="#475be8"
           color="#fcfcfc"
           icon={<Add />}
         />
       </Stack>
 
-      <Box mt="20px" sx={{ display: 'flex', flexWrap: 'wrap' }}>
-
-      </Box>
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}></Box>
     </Box>
   );
 };
