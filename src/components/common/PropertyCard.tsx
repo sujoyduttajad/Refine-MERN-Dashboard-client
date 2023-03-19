4e52bd88a436d850a15b5337eb6119a9e7da679f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-19 17:48:27 +0530

    Update PropertyCard.tsx

--- src/components/common/PropertyCard.tsx
+++ src/components/common/PropertyCard.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Place } from "@mui/icons-material";
+import { Link } from "@pankod/refine-mui";
+import {
+  Typography,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  Stack,
+} from "@pankod/refine-mui";
 
-const PropertyCard = () => {
+import { PropertyCardProps } from "interfaces/property";
+
+const PropertyCard = ({
+  id,
+  title,
+  price,
+  location,
+  photo,
+}: PropertyCardProps) => {
   return (
-    <div>PropertyCard</div>
-  )
-}
+    <Card
+      component={Link}
+      to={`/properties/${id}`}
+      sx={{
+        maxWidth: "330px",
+        padding: "10px",
+        cursor: "pointer",
+        "&:hover": {
+          boxShadow: "0 22px 45px 2px rgba(176, 176, 176, 0.1)",
+        },
+      }}
+    >
+      <CardMedia
+        component="img"
+        width="100%"
+        height={210}
+        image={photo}
+        alt="card-image"
+        sx={{
+          borderRadius: "10px",
+        }}
+      />
+    </Card>
+  );
+};
 
-export default PropertyCard
+export default PropertyCard;
