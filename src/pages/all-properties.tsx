e0d397255966fc12620c9c0495329648545439eb
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-19 17:48:25 +0530

    Update all-properties.tsx

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -13,8 +13,8 @@ const AllProperties = () => {
 
   const allProperties = data?.data ?? [];
 
-  if(isLoading) return <Typography>Loading...</Typography>;
-  if(isError) return <Typography>Error...</Typography>;
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Error...</Typography>;
 
   return (
     <Box>
@@ -32,19 +32,16 @@ const AllProperties = () => {
       </Stack>
 
       <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
-        {
-          allProperties.map(property => (
-            <PropertyCard 
-              key={property._id}
-              id={property._id}
-              title={property.title}
-              price={property.price}
-              location={property.location}
-              photo={property.photo}
-              
-            />
-          ))
-        }
+        {allProperties.map((property) => (
+          <PropertyCard
+            key={property._id}
+            id={property._id}
+            title={property.title}
+            price={property.price}
+            location={property.location}
+            photo={property.photo}
+          />
+        ))}
       </Box>
     </Box>
   );
