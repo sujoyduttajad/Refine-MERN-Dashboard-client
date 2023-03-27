7ee81844c75f7d43b0fc77bb0742b02048b0c9e1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-27 19:11:34 +0530

    Update Loading&Error.tsx

--- src/components/common/Loading&Error.tsx
+++ src/components/common/Loading&Error.tsx
@@ -1,30 +1,46 @@
-import { Box, Typography } from '@pankod/refine-mui'
-import React from 'react'
+import { Box, Typography } from "@pankod/refine-mui";
+import React from "react";
 
-export  function Loading() {
+export function Loading() {
   return (
     <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexWrap: "wrap",
-          gap: 3,
-          padding: "3rem",
-          width: "100%",
-          borderRadius: "10px",
-          backgroundColor: "#E7E9FC",
-        }}
-      >
-        <Typography variant="h3" fontSize={25} fontWeight={700} color="#11142d">
-          Loading...
-        </Typography>
-      </Box>
-  )
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        gap: 3,
+        padding: "3rem",
+        width: "100%",
+        borderRadius: "10px",
+        backgroundColor: "#E7E9FC",
+      }}
+    >
+      <Typography variant="h3" fontSize={25} fontWeight={700} color="#11142d">
+        Loading...
+      </Typography>
+    </Box>
+  );
 }
 
 export function Error() {
-    return (
-      <div>Loading&Error</div>
-    )
-  }
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        gap: 3,
+        padding: "3rem",
+        width: "100%",
+        borderRadius: "10px",
+        backgroundColor: "#E7E9FC",
+      }}
+    >
+      <Typography variant="h3" fontSize={25} fontWeight={700} color="#E84558">
+        Oops we have an Error
+      </Typography>
+    </Box>
+  );
+}
