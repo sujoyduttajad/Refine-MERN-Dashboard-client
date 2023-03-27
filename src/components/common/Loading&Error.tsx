d206775b28ea91b28ff6d87f0044e4db464cdd72
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-27 19:07:32 +0530

    Create Loading&Error.tsx

---
+++ src/components/common/Loading&Error.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export  function Loading() {
+  return (
+    <div>Loading&Error</div>
+  )
+}
+
+export function Error() {
+    return (
+      <div>Loading&Error</div>
+    )
+  }
