1e0d315c70219fa15c75e09175ff0c632c9a1024
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-28 16:57:45 +0530

    Update property-details.tsx

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Typography, Box, Stack } from '@pankod/refine-mui'
+import { useDelete, useGetIdentity, useShow } from '@pankod/refine-core'
+import { useParams, useNavigate } from '@pankod/refine-react-router-v6';
+import { ChatBubble, Delete, Edit, Phone, Place, Star } from '@mui/icons-material';
+import { CustomButton } from 'components';
 
 const PropertyDetails = () => {
+  const navigate = useNavigate();
   return (
     <div>PropertyDetails</div>
   )
