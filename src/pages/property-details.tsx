ce28831824fa92a7f36ee6baa65d360fa9ff321b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-01 18:25:04 +0530

    Update property-details.tsx

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -48,12 +48,24 @@ const PropertyDetails = () => {
         gap={4}
       >
         <Box flex={1} maxWidth={764}>
-          <img 
+          <img
             src={propertyDetails.photo}
             alt={propertyDetails.title}
-            style={{ objectFit: "cover", borderRadius: '10px'}}
+            style={{ objectFit: "cover", borderRadius: "10px" }}
             className="property_details-img"
           />
+
+          <Box mt="15px">
+            <Stack>
+              <Typography>{propertyDetails.propertyType}</Typography>
+              <Box>
+                {/* Challenge is to make Rating dynamic */}
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <Star key={`star-${star}`} sx={{ color: "#f2c94c" }} />
+                ))}
+              </Box>
+            </Stack>
+          </Box>
         </Box>
       </Box>
     </Box>
