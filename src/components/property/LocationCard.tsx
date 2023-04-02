626a46cbc4628fffe8a52346d492379127de7a1f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-02 19:16:46 +0530

    Update LocationCard.tsx

--- src/components/property/LocationCard.tsx
+++ src/components/property/LocationCard.tsx
@@ -1,39 +1,24 @@
-import {
-    Card,
-    CardContent,
-    CardMedia,
-  } from "@pankod/refine-mui";
-  import mapNY from "../../assets/mapNY.png";
+import { Card, CardMedia } from "@pankod/refine-mui";
+import mapNY from "../../assets/mapNY.png";
 
-  const LocationCard = () => {
-    return (
-      <Card
-        variant="outlined"
-        raised={true}
-        sx={{
-          minWidth: 275,
-        //   width: "100%",
-          borderRadius: "20px",
-        }}
-      >
-        {/* <CardContent
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        > */}
-          <CardMedia
-            component="img"
-            image={mapNY}
-            alt="Location geo map"
-            sx={{ height: "100%", width: "22rem", margin: 0 }}
-          />
-       
-        {/* </CardContent> */}
-      </Card>
-    );
-  };
-  
-  export default LocationCard;
-  
+const LocationCard = () => {
+  return (
+    <Card
+      variant="outlined"
+      raised={true}
+      sx={{
+        minWidth: 275,
+        borderRadius: "20px",
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={mapNY}
+        alt="Location geo map"
+        sx={{ height: "100%", width: "22rem", margin: 0 }}
+      />
+    </Card>
+  );
+};
+
+export default LocationCard;
