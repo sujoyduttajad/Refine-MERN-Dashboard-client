fe192321fb4ee2349ece33f6f217b8ad68b8db1e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-02 15:45:53 +0530

    Update property-details.tsx

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -26,7 +26,7 @@ import {
 import { CustomButton } from "components";
 import { Error, Loading } from "components/common/Loading&Error";
 import { formatter } from "utils/functions";
-import sampleAgent from "../assets/sample-agent.jpg";
+import AgentCard from "components/property/AgentCard";
 
 const PropertyDetails = () => {
   const navigate = useNavigate();
@@ -296,70 +296,7 @@ const PropertyDetails = () => {
           ml={2}
           aria-label="agent-details"
         >
-          <Card
-            variant="outlined"
-            sx={{
-              minWidth: 275,
-              width: "100%",
-              padding: "1rem",
-              display: "flex",
-              flexDirection: "column",
-              borderRadius: "",
-            }}
-          >
-            <CardContent
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <CardMedia
-                component="img"
-                image={sampleAgent}
-                alt="Agent Profile picture"
-                sx={{ height: "7rem", width: "7rem", borderRadius: "50%" }}
-              />
-              <Typography mt={2} fontSize={16} fontWeight={600}>
-                Cassandra Wilson
-              </Typography>
-              <Typography fontSize={14} fontWeight={400} color="#808191">
-                Agent
-              </Typography>
-              <Stack direction="row" mt={0.5} alignItems="center" gap={0.5}>
-                <Place sx={{ color: "#808191" }} />
-                <Typography fontSize={14} color="#808191">
-                  New York, USA
-                </Typography>
-              </Stack>
-              <Typography mt={2} fontSize={16} fontWeight={600}>
-                10 Properties
-              </Typography>
-
-              <Stack direction="row" mt={3} alignItems="center" gap={2}>
-                <CustomButton
-                  type="button"
-                  title="Message"
-                  backgroundColor="#475be8"
-                  color="#fcfcfc"
-                  heightValue="40px"
-                  paddingValue="1px 10px"
-                  fontSizeValue="16.5px"
-                  icon={<ChatOutlined />}
-                />
-                <CustomButton
-                  type="button"
-                  title="Call"
-                  backgroundColor="#2ED480"
-                  color="#fcfcfc"
-                  heightValue="40px"
-                  paddingValue="1px 10px"
-                  fontSizeValue="16.5px"
-                  icon={<Phone />}
-                />
-              </Stack>
-            </CardContent>
-          </Card>
+          <AgentCard />
           <CustomButton
             type="button"
             title="Book Now"
