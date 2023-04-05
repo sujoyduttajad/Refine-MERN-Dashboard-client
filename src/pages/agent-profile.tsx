ea561b139ca230d5992c10af046aa44def49d453
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-05 14:01:26 +0530

    Update agent-profile.tsx

--- src/pages/agent-profile.tsx
+++ src/pages/agent-profile.tsx
@@ -1,14 +1,8 @@
-import React from "react";
-import { useList } from "@pankod/refine-core";
-import { Box, Typography } from "@pankod/refine-mui";
-import AgentCard from "components/property/AgentCard";
+import React from "react"
+
 
 const AgentProfile = () => {
-  const { data, isLoading, isError } = useList({
-    resource: "api/v1/users",
-  });
 
-  const allAgents = data?.data ?? [];
 
   return <div>agent-profile</div>;
 };
