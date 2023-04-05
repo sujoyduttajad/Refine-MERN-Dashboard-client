539b5ae637a32c94cd1772bc8cf489d058e3f6e3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-05 16:59:05 +0530

    Update AgentCard.tsx

--- src/components/agent/AgentCard.tsx
+++ src/components/agent/AgentCard.tsx
@@ -1,4 +1,9 @@
 import { AgentCardProp } from "interfaces/agent";
+import { EmailOutlined, LocationCity, Phone, Place } from "@mui/icons-material";
+import { useGetIdentity } from "@pankod/refine-core";
+import { Box, Stack, Typography } from "@pankod/refine-mui";
+import { Link } from "@pankod/refine-react-router-v6";
+
 
 const AgentCard = ({
   id,
