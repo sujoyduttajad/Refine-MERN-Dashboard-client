6db44ff775177ec974e995bed7441b1324a1c078
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-05 17:13:35 +0530

    Update AgentCard.tsx

--- src/components/agent/AgentCard.tsx
+++ src/components/agent/AgentCard.tsx
@@ -4,7 +4,6 @@ import { useGetIdentity } from "@pankod/refine-core";
 import { Box, Stack, Typography } from "@pankod/refine-mui";
 import { Link } from "@pankod/refine-react-router-v6";
 
-
 const AgentCard = ({
   id,
   name,
@@ -12,19 +11,28 @@ const AgentCard = ({
   avatar,
   noOfProperties,
 }: AgentCardProp) => {
-
   const { data: currentUser } = useGetIdentity();
 
   const generateLink = () => {
-    if(currentUser.email === email) return '/my-profile'
+    if (currentUser.email === email) return "/my-profile";
 
-    return `/agent/show/${id}`
-  }
+    return `/agent/show/${id}`;
+  };
 
   return (
     <Box
       component={Link}
       to={generateLink()}
+      width="100%"
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", sm: "row" },
+        padding: "20px",
+        gap: "20px",
+        "&:hover": {
+          boxShadow: "0 22px 45px 2px rgba(176, 176, 176, 0.1)",
+        },
+      }}
     >
       AgentCard
     </Box>
