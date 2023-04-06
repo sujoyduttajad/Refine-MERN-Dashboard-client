4edcf1e80b836f076fa8465daa1fa9318aaf6100
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-06 13:19:52 +0530

    Update InfoBar.tsx

--- src/components/agent/InfoBar.tsx
+++ src/components/agent/InfoBar.tsx
@@ -1,13 +1,15 @@
-import { InfoBarProps } from 'interfaces/agent'
+import { InfoBarProps } from "interfaces/agent";
 import { Box, Stack, Typography } from "@pankod/refine-mui";
 
-const InfoBar = ({ icon, content}: InfoBarProps) => {
+const InfoBar = ({ icon, content }: InfoBarProps) => {
   return (
     <Stack direction="row" justifyContent="space-between" gap={5} mr="8%">
-        <Box>{icon}</Box>
-        <Typography>{content}</Typography>
+      <Box>{icon}</Box>
+      <Typography fontSize={14} color="#808191">
+        {content}
+      </Typography>
     </Stack>
-  )
-}
+  );
+};
 
-export default InfoBar
+export default InfoBar;
