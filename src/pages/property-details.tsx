c5b0183cff0eb25f137f18e68b1d4728a137b62e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-06 17:27:53 +0530

    Update property-details.tsx

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Typography, Box, Stack } from "@pankod/refine-mui";
 import { useDelete, useGetIdentity, useShow } from "@pankod/refine-core";
 import { useParams, useNavigate } from "@pankod/refine-react-router-v6";
@@ -14,7 +14,7 @@ import {
   Star,
   WifiRounded,
 } from "@mui/icons-material";
-import { CustomButton } from "components";
+import { CustomButton, Facilities } from "components";
 import { Error, Loading } from "components/common/Loading&Error";
 import { formatter } from "utils/functions";
 
@@ -207,128 +207,7 @@ const PropertyDetails = () => {
                 flexWrap="wrap"
                 mt={5}
               >
-                <Box width="100%" display="flex" flexDirection="column">
-                  <Typography
-                    fontSize={22}
-                    fontWeight={600}
-                    color="#11142d"
-                    textTransform="capitalize"
-                  >
-                    Facillity
-                  </Typography>
-                  {/* 1st row of Facillities */}
-                  <Box
-                    aria-label="facility-column"
-                    display="flex"
-                    flexDirection="row"
-                    justifyContent="space-between"
-                    gap={5}
-                    mt={3}
-                    flexWrap="wrap"
-                  >
-                    <Stack
-                      direction="row"
-                      alignItems="center"
-                      gap={2}
-                      flexWrap="nowrap"
-                    >
-                      <KingBedOutlined sx={{ color: "#808191" }} />
-                      <Typography
-                        fontSize={18}
-                        fontWeight={500}
-                        color="#11142d"
-                      >
-                        4 Beds
-                      </Typography>
-                    </Stack>
-                    <Stack
-                      direction="row"
-                      alignItems="center"
-                      gap={2}
-                      flexWrap="nowrap"
-                    >
-                      <BathtubOutlined sx={{ color: "#808191" }} />
-                      <Typography
-                        fontSize={18}
-                        fontWeight={500}
-                        color="#11142d"
-                      >
-                        2 Baths
-                      </Typography>
-                    </Stack>
-                    <Stack
-                      direction="row"
-                      alignItems="center"
-                      gap={2}
-                      flexWrap="nowrap"
-                    >
-                      <SquareFootOutlined sx={{ color: "#808191" }} />
-                      <Typography
-                        fontSize={18}
-                        fontWeight={500}
-                        color="#11142d"
-                      >
-                        28M Area
-                      </Typography>
-                    </Stack>
-                  </Box>
-                  {/* 2nd Row of Facillities */}
-                  <Box
-                    aria-label="facility-column2"
-                    display="flex"
-                    flexDirection="row"
-                    justifyContent="space-between"
-                    gap={5}
-                    mt={3}
-                    flexWrap="wrap"
-                  >
-                    <Stack
-                      direction="row"
-                      alignItems="center"
-                      gap={2}
-                      flexWrap="nowrap"
-                    >
-                      <KitchenOutlined sx={{ color: "#808191" }} />
-                      <Typography
-                        fontSize={18}
-                        fontWeight={500}
-                        color="#11142d"
-                      >
-                        1 Kitchen
-                      </Typography>
-                    </Stack>
-                    <Stack
-                      direction="row"
-                      alignItems="center"
-                      gap={2}
-                      flexWrap="nowrap"
-                    >
-                      <BalconyOutlined sx={{ color: "#808191" }} />
-                      <Typography
-                        fontSize={18}
-                        fontWeight={500}
-                        color="#11142d"
-                      >
-                        1 Balcony
-                      </Typography>
-                    </Stack>
-                    <Stack
-                      direction="row"
-                      alignItems="center"
-                      gap={2}
-                      flexWrap="nowrap"
-                    >
-                      <WifiRounded sx={{ color: "#808191" }} />
-                      <Typography
-                        fontSize={18}
-                        fontWeight={500}
-                        color="#11142d"
-                      >
-                        Wifi included
-                      </Typography>
-                    </Stack>
-                  </Box>
-                </Box>
+                <Facilities />
                 {/* Description */}
                 <Box width="100%" display="flex" flexDirection="column" mt={5}>
                   <Typography
