2f161d241c6ce86df690365feb0600c1c2924c90
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-06 12:58:54 +0530

    Create InfoBar.tsx

---
+++ src/components/agent/InfoBar.tsx
@@ -0,0 +1,10 @@
+import { InfoBarProps } from 'interfaces/agent'
+import React from 'react'
+
+const InfoBar = ({ icon, name}: InfoBarProps) => {
+  return (
+    <div>InfoBar</div>
+  )
+}
+
+export default InfoBar
