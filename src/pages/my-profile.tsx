12d186f31a417a7d7e17d9485423135f14e8806e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-07 12:58:22 +0530

    Update my-profile.tsx

--- src/pages/my-profile.tsx
+++ src/pages/my-profile.tsx
@@ -1,9 +1,21 @@
 import { useOne, useGetIdentity } from "@pankod/refine-core";
 import { Box } from "@pankod/refine-mui";
 import { Profile } from "components";
+import { Error, Loading } from "components/common/Loading&Error";
 
 
 const MyProfile = () => {
+  const { data: user } = useGetIdentity();
+  const { data, isLoading, isError } = useOne({
+    resource: "users",
+    id: user?.userid,
+  });
+
+  const myProfile = data?.data ?? [];
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
+
   return (
     <Box>
       <Profile />
