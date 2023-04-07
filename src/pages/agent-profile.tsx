9439417b4954d8f5d41c2d7800e7dd524567550b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-07 22:55:10 +0530

    Update agent-profile.tsx

--- src/pages/agent-profile.tsx
+++ src/pages/agent-profile.tsx
@@ -1,9 +1,20 @@
-import React from "react"
+import { useOne, useGetIdentity } from "@pankod/refine-core";
+import { Profile } from "components";
+import { Error, Loading } from "components/common/Loading&Error";
 
 
 const AgentProfile = () => {
+  const { data: user } = useGetIdentity();
+  const { data, isLoading, isError } = useOne({
+    resource: "users",
+    id: user?.userid,
+  });
 
+  const myProfile = data?.data ?? [];
 
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
+  
   return <div>agent-profile</div>;
 };
 
