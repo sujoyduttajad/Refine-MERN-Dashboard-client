ecb6e7e3c4bdf2679d293550547eff08e06583c3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-08 19:28:11 +0530

    Update CustomerCard.tsx

--- src/components/home/CustomerCard.tsx
+++ src/components/home/CustomerCard.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Box, Stack, Typography } from '@pankod/refine-mui'
 
 const CustomerCard = () => {
   return (
-    <div>CustomerCard</div>
+    <Box display="flex" justifyContent="space-evenly" gap={1}>
+        <Stack direction="column">
+            <Typography fontSize={14} color="#808191">Total Customers</Typography>
+            <Typography variant='h4'>500K</Typography>
+            <Typography fontSize={12} color="#18C346">21.77%</Typography>
+        </Stack>
+        <Stack></Stack>
+    </Box>
   )
 }
 
