cf9de25299e6e94148fe3a75a8cc282e7f3036f8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-08 23:42:40 +0530

    Update SalesCard.tsx

--- src/components/home/SalesCard.tsx
+++ src/components/home/SalesCard.tsx
@@ -1,13 +1,19 @@
 import { Box, CardMedia, Stack, Typography } from "@pankod/refine-mui";
 import { PropertyListValues } from "interfaces/property";
+import { formatter } from "utils/functions";
 
-const SalesCard = ({ creator, title, photo, price, location  }: PropertyListValues ) => {
-    
+const SalesCard = ({
+  creator,
+  title,
+  photo,
+  price,
+  location,
+}: PropertyListValues) => {
+  const priceDisplay = formatter.format(price).slice(0, -3);
   return (
-   
-      <Box display="flex" justifyContent="space-between">
-        <Stack direction="row" justifyContent="flex-start">
-          <CardMedia 
+    <Box display="flex" justifyContent="space-between">
+      <Stack direction="row" justifyContent="flex-start" gap={2}>
+        <CardMedia
           component="img"
           image={photo}
           alt={title}
@@ -17,15 +23,27 @@ const SalesCard = ({ creator, title, photo, price, location  }: PropertyListValu
             borderRadius: "20px",
             objectFit: "cover",
           }}
-          />
-          <Stack>
-            <Typography>{title}</Typography>
-            <Typography></Typography>
-          </Stack>
+        />
+        <Stack direction="column" gap={1}>
+          <Typography fontSize={18} fontWeight={600} color="#11142d">
+            {title}
+          </Typography>
+          <Box
+            px={1.5}
+            py={0.5}
+            width="fit-content"
+            borderRadius={1}
+            bgcolor="#dadefa"
+            // height="100%"
+          >
+            <Typography fontSize={16} fontWeight={700} color="#475be8">
+              {priceDisplay}
+            </Typography>
+          </Box>
         </Stack>
-        <Stack></Stack>
-      </Box>
-   
+      </Stack>
+      <Stack></Stack>
+    </Box>
   );
 };
 
