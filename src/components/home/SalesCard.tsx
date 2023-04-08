11f0e1250bc67d728e25f38791d4f857069f7012
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-08 23:27:33 +0530

    Update SalesCard.tsx

--- src/components/home/SalesCard.tsx
+++ src/components/home/SalesCard.tsx
@@ -1,20 +1,20 @@
-import { Box, CardMedia, Stack, Typography } from "@pankod/refine-mui"
-
+import { Box, CardMedia, Stack, Typography } from "@pankod/refine-mui";
 
 const SalesCard = () => {
   return (
     <Box>
-
-        <Box>
-            <CardMedia />
-            <Stack>
-                <Typography></Typography>
-                <Typography></Typography>
-            </Stack>
-            <Stack></Stack>
-        </Box>
+      <Box display="flex" justifyContent="space-between">
+        <Stack direction="row" justifyContent="flex-start">
+          <CardMedia />
+          <Stack>
+            <Typography>Metro Jay Apartment</Typography>
+            <Typography></Typography>
+          </Stack>
+        </Stack>
+        <Stack></Stack>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default SalesCard
+export default SalesCard;
