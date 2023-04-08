f41c0593b058cfee40c0d6402617de628dc60b72
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-08 23:57:46 +0530

    Update ThreeDotsMenu.tsx

--- src/components/common/ThreeDotsMenu.tsx
+++ src/components/common/ThreeDotsMenu.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { MoreVert } from "@mui/icons-material";
+import { IconButton, Menu, MenuItem, Stack } from "@pankod/refine-mui";
 
-const ThreeDotsMenu = () => {
+const ITEM_HEIGHT = 48;
+
+const ThreeDotsMenu = ({ option, open, anchorEl, handleClick, handleClose}) => {
   return (
-    <div>ThreeDotsMenu</div>
-  )
-}
+    <Stack>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? "long-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVert />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          "aria-labelledby": "long-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: "20ch",
+          },
+        }}
+      >
+        <MenuItem selected={option === "More Detail"} onClick={handleClose}>
+          More Details
+        </MenuItem>
+      </Menu>
+    </Stack>
+  );
+};
 
-export default ThreeDotsMenu
+export default ThreeDotsMenu;
