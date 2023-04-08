2002444e323055a2a336c710780b475f02507461
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-08 20:23:45 +0530

    Update TopAgent.tsx

--- src/components/home/TopAgent.tsx
+++ src/components/home/TopAgent.tsx
@@ -1,17 +1,48 @@
-import { Box, CardMedia, Stack, Typography } from '@pankod/refine-mui'
+import { Box, CardMedia, Stack, Typography } from "@pankod/refine-mui";
+import { useList } from "@pankod/refine-core";
+import { Error, Loading } from "components/common/Loading&Error";
 
 const TopAgent = () => {
-  
+  const { data, isLoading, isError } = useList({
+    resource: "users",
+  });
+
+  const allAgents = data?.data ?? [];
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
+
+  console.log(allAgents);
   return (
     <Box>
-      <Stack>
-        <CardMedia />
-        <Stack>
-          <Typography></Typography>
-        </Stack>
-      </Stack>
+      {allAgents.map((agent) => (
+        <Box>
+          <Stack>
+            <CardMedia
+              component="img"
+              image={agent.avatar}
+              alt={agent.name}
+              width={90}
+              height={90}
+              sx={{
+                borderRadius: "50%",
+                objectFit: "cover",
+              }}
+            />
+            <Stack>
+              <Typography
+                textTransform="capitalize"
+                fontSize={14}
+                fontWeight={600}
+              >
+                {agent.name}
+              </Typography>
+            </Stack>
+          </Stack>
+        </Box>
+      ))}
     </Box>
-  )
-}
+  );
+};
 
-export default TopAgent
+export default TopAgent;
