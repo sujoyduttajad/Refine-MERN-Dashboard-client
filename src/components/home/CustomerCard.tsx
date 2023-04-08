b291f7374aeaa01f25660bd2fc2fb362e790b925
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-08 19:33:56 +0530

    Update CustomerCard.tsx

--- src/components/home/CustomerCard.tsx
+++ src/components/home/CustomerCard.tsx
@@ -1,16 +1,34 @@
-import { Box, Stack, Typography } from '@pankod/refine-mui'
+import { Box, Stack, Typography } from "@pankod/refine-mui";
 
 const CustomerCard = () => {
   return (
-    <Box display="flex" justifyContent="space-evenly" gap={1}>
-        <Stack direction="column">
-            <Typography fontSize={14} color="#808191">Total Customers</Typography>
-            <Typography variant='h4'>500K</Typography>
-            <Typography fontSize={12} color="#18C346">21.77%</Typography>
+    <Box>
+      <Box display="flex" justifyContent="space-evenly" gap={1}>
+        <Stack direction="column" gap={2}>
+          <Typography fontSize={14} color="#808191">
+            Total Customers
+          </Typography>
+          <Typography variant="h4">500K</Typography>
+          <Typography fontSize={12} fontWeight={700} color="#18C346">
+            21.77%
+          </Typography>
         </Stack>
         <Stack></Stack>
+      </Box>
+      <Box display="flex" justifyContent="space-evenly" gap={1}>
+        <Stack direction="column" gap={2}>
+          <Typography fontSize={14} color="#808191">
+            Total Customers
+          </Typography>
+          <Typography variant="h4">500K</Typography>
+          <Typography fontSize={12} fontWeight={700} color="#18C346">
+            21.77%
+          </Typography>
+        </Stack>
+        <Stack></Stack>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default CustomerCard
+export default CustomerCard;
