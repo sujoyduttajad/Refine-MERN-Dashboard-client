5ea1e123a28ef2cb7f8940b911f206535d1fe2af
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-08 23:46:41 +0530

    Update SalesCard.tsx

--- src/components/home/SalesCard.tsx
+++ src/components/home/SalesCard.tsx
@@ -1,6 +1,19 @@
-import { Box, CardMedia, Stack, Typography } from "@pankod/refine-mui";
+import React from "react";
+import { MoreVert } from "@mui/icons-material";
+import {
+  Box,
+  CardMedia,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography,
+} from "@pankod/refine-mui";
 import { PropertyListValues } from "interfaces/property";
 import { formatter } from "utils/functions";
+
+const ITEM_HEIGHT = 48;
+const option = "More Detail";
 
 const SalesCard = ({
   creator,
@@ -9,6 +22,15 @@ const SalesCard = ({
   price,
   location,
 }: PropertyListValues) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const priceDisplay = formatter.format(price).slice(0, -3);
   return (
     <Box display="flex" justifyContent="space-between">
@@ -34,7 +56,6 @@ const SalesCard = ({
             width="fit-content"
             borderRadius={1}
             bgcolor="#dadefa"
-            // height="100%"
           >
             <Typography fontSize={16} fontWeight={700} color="#475be8">
               {priceDisplay}
@@ -42,7 +63,37 @@ const SalesCard = ({
           </Box>
         </Stack>
       </Stack>
-      <Stack></Stack>
+      <Stack>
+        <IconButton
+          aria-label="more"
+          id="long-button"
+          aria-controls={open ? "long-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <MoreVert />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            "aria-labelledby": "long-button",
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: "20ch",
+            },
+          }}
+        >
+          <MenuItem selected={option === "More Detail"} onClick={handleClose}>
+            More Details
+          </MenuItem>
+        </Menu>
+      </Stack>
     </Box>
   );
 };
