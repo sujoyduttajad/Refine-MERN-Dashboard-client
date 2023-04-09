b35f39e541b87d3613651f9e8eaebc68fd576859
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-09 17:19:00 +0530

    Update home.d.ts

--- src/interfaces/home.d.ts
+++ src/interfaces/home.d.ts
@@ -4,3 +4,18 @@ export interface PieChartProps {
     series: Array<number>;
     colors: Array<string>;
 }
+
+export interface ApexAxisChartSeries {
+    name: string;
+    data: number[];
+}
+
+export interface ApexNonAxisChartSeries {
+    name: string;
+    data: number;
+}
+
+export interface CustomerCardProps {
+    data: Object;
+    series: (ApexAxisChartSeries | ApexNonAxisChartSeries)[];
+}
