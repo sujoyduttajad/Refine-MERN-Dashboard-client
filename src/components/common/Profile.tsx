4b48514fa047dd59fd55b048b600ca0261cc6a68
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-09 22:31:12 +0530

    Update Profile.tsx

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -85,7 +85,7 @@ const Profile = ({ type, name, avatar, email, properties }: ProfileProps) => (
                     {name}
                   </Typography>
                   <Typography fontSize={16} color="#808191">
-                    Real-Estate Agent
+                    CEO
                   </Typography>
                 </Stack>
                 {/* <CustomButton
