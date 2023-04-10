6e08ddbb36e724f1ce31dea5fc0e87527c6f8eb9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-10 13:59:18 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,9 +5,11 @@ import evoiaDark from './evoiaDark.svg';
 import arrowUp from './arrowUp.svg';
 import skyscrapper from './skyscrapper-violet.jpg'
 import pointyBuilding from './login-page-image.png'
+import BrandIcon from './Brand-Light.svg'
 
 export {
   logo,
+  BrandIcon,
   yariga,
   evoiaLight,
   evoiaDark,
