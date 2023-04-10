dd83f855d637b205c1c4a541cf4d62b3842fc942
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-11 02:58:50 +0530

    Update Profile.tsx

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -85,7 +85,7 @@ const Profile = ({ type, name, avatar, email, properties }: ProfileProps) => (
                     {name}
                   </Typography>
                   <Typography fontSize={16} color="#808191">
-                    Admin
+                    { name === 'sujoy dutta' ? 'Admin' : 'Real-Estate Agent'}
                   </Typography>
                 </Stack>
                 {/* <CustomButton
