948c07d709b5453bf03b9b9c63ae2c5f58be2a80
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-12 11:24:18 +0530

    Create infoContent.js

---
+++ src/utils/infoContent.js
@@ -0,0 +1,10 @@
+/* DO NOT DELETE Unless you want to use the app as a template, then just change the content */
+// This is particularly for the Help&Info page
+
+export const companyInfo = {
+  content:
+    `Evoia is a Property management platform where Properties can be uploaded 
+    by agents and then could be used for selling or advertising the properties. 
+    Real-Estate agents can login to the app with their Google account and quickly create their profile
+    and start uploading properties. A step-by-step guide is also provided below.`,
+};
