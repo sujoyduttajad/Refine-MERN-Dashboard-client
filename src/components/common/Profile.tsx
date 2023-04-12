e6d9276983fb7a2a08328bd1e7319282d86f1909
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-12 11:07:24 +0530

    Update Profile.tsx

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -52,7 +52,8 @@ const Profile = ({ type, name, avatar, email, properties }: ProfileProps) => (
           >
             <img
               src={
-                checkImage(avatar)
+                // checkImage(avatar)
+                avatar
                   ? avatar
                   : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png"
               }
