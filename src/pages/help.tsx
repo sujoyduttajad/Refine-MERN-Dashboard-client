a619c373ed15bd4317a0ef723d53689c280a386a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-12 14:25:45 +0530

    Update help.tsx

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,7 +1,6 @@
 import { useOne } from "@pankod/refine-core";
 import { Box, CardMedia, Stack, Typography } from "@pankod/refine-mui";
 import { evoiaDark } from "assets";
-import InfoSection from "components/common/InfoSection";
 import { Error, Loading } from "components/common/Loading&Error";
 import { companyInfo } from "utils/infoContent";
 
@@ -22,13 +21,41 @@ const HelpAndInfo = () => {
         Help & Information
       </Typography>
       {/* Company */}
-    
-      <InfoSection 
-        header="What is Evoia?"
-        content={companyInfo.content}
-        photo={evoiaDark}
-        title="Brand logo"
-      />
+      <Box
+        mt={2.5}
+        maxWidth="65rem"
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#fafafa"
+      >
+        <Typography variant="h5" fontWeight={500}>
+          What is Evoia?
+        </Typography>
+        <Stack direction={{ xs: "column", sm: "row" }} mt={3}>
+          <Typography
+            variant="body1"
+            flexWrap="wrap"
+            maxWidth="40rem"
+            width="100%"
+            pr={{ xs: 0, sm: 2 }}
+            pt={{ xs: 2, sm: 0 }}
+          >
+            {companyInfo.content}
+          </Typography>
+          <Box>
+            <CardMedia
+              component="img"
+              image={evoiaDark}
+              alt={"Brand logo"}
+              height={120}
+              sx={{
+                borderRadius: "5px",
+                objectFit: "contain",
+              }}
+            />
+          </Box>
+        </Stack>
+      </Box>
       {/* Creator */}
       <Box
         mt={2.5}
@@ -71,13 +98,6 @@ const HelpAndInfo = () => {
           </Box>
         </Stack>
       </Box>
-      <InfoSection 
-        header="About the Creator"
-        content={companyInfo.aboutMe}
-        photo={owner?.avatar}
-        title={owner?.name}
-        flexDir="row-reverse"
-      />
     </Box>
   );
 };
