00f720abcd46bd254e0b2f21faea9d6d51800463
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-12 11:54:41 +0530

    Update help.tsx

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,13 +1,27 @@
+import { useOne } from "@pankod/refine-core";
 import { Box, CardMedia, Stack, Typography } from "@pankod/refine-mui";
 import { evoiaDark } from "assets";
+import { Error, Loading } from "components/common/Loading&Error";
 import { companyInfo } from "utils/infoContent";
 
 const HelpAndInfo = () => {
+    const { data, isLoading, isError } = useOne({
+        resource: "users",
+        id: "64117ddfcf0c47761e01b1c4",
+      });
+
+      const owner = data?.data ?? [];
+
+      if (isLoading) return <Loading />;
+      if (isError) return <Error />;
+
+
   return (
     <Box>
       <Typography fontSize={25} fontWeight={700} color="#11142d">
         Help & Information
       </Typography>
+      {/* Company */}
       <Box
         mt={2.5}
         maxWidth="65rem"
@@ -15,30 +29,67 @@ const HelpAndInfo = () => {
         padding="20px"
         bgcolor="#fafafa"
       >
-        <Typography variant="h5" fontWeight={500}>What is Evoia?</Typography>
+        <Typography variant="h5" fontWeight={500}>
+          What is Evoia?
+        </Typography>
         <Stack direction={{ xs: "column", sm: "row" }} mt={3}>
           <Typography
             variant="body1"
             flexWrap="wrap"
             maxWidth="40rem"
             width="100%"
-            pr={{xs: 0, sm: 2}}
-            pt={{xs: 2, sm: 0}}
+            pr={{ xs: 0, sm: 2 }}
+            pt={{ xs: 2, sm: 0 }}
           >
             {companyInfo.content}
           </Typography>
           <Box>
-          <CardMedia
-            component="img"
-            image={evoiaDark}
-            alt={"Brand logo"}
-            // width={70}
-            height={120}
-            sx={{
-              borderRadius: "5px",
-              objectFit: "contain",
-            }}
-          />
+            <CardMedia
+              component="img"
+              image={evoiaDark}
+              alt={"Brand logo"}
+              height={120}
+              sx={{
+                borderRadius: "5px",
+                objectFit: "contain",
+              }}
+            />
+          </Box>
+        </Stack>
+      </Box>
+      {/* Creator */}
+      <Box
+        mt={2.5}
+        maxWidth="65rem"
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#fafafa"
+      >
+        <Typography variant="h5" fontWeight={500}>
+          About the Creator
+        </Typography>
+        <Stack direction={{ xs: "column", sm: "row-reverse" }}  mt={3}>
+          <Typography
+            variant="body1"
+            flexWrap="wrap"
+            maxWidth="40rem"
+            width="100%"
+            pr={{ xs: 0, sm: 2 }}
+            pt={{ xs: 2, sm: 0 }}
+          >
+            {companyInfo.content}
+          </Typography>
+          <Box>
+            <CardMedia
+              component="img"
+              image={evoiaDark}
+              alt={"Brand logo"}
+              height={120}
+              sx={{
+                borderRadius: "5px",
+                objectFit: "contain",
+              }}
+            />
           </Box>
         </Stack>
       </Box>
