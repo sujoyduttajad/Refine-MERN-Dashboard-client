7195992dc80a3fc6e1b81b7e668ee3c9c5d0a990
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-12 14:08:05 +0530

    Update help.tsx

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -22,41 +22,7 @@ const HelpAndInfo = () => {
         Help & Information
       </Typography>
       {/* Company */}
-      <Box
-        mt={2.5}
-        maxWidth="65rem"
-        borderRadius="15px"
-        padding="20px"
-        bgcolor="#fafafa"
-      >
-        <Typography variant="h5" fontWeight={500}>
-          What is Evoia?
-        </Typography>
-        <Stack direction={{ xs: "column", sm: "row" }} mt={3}>
-          <Typography
-            variant="body1"
-            flexWrap="wrap"
-            maxWidth="40rem"
-            width="100%"
-            pr={{ xs: 0, sm: 2 }}
-            pt={{ xs: 2, sm: 0 }}
-          >
-            {companyInfo.content}
-          </Typography>
-          <Box>
-            <CardMedia
-              component="img"
-              image={evoiaDark}
-              alt={"Brand logo"}
-              height={120}
-              sx={{
-                borderRadius: "5px",
-                objectFit: "contain",
-              }}
-            />
-          </Box>
-        </Stack>
-      </Box>
+    
       <InfoSection 
         header="What is Evoia?"
         content={companyInfo.content}
@@ -105,6 +71,13 @@ const HelpAndInfo = () => {
           </Box>
         </Stack>
       </Box>
+      <InfoSection 
+        header="About the Creator"
+        content={companyInfo.aboutMe}
+        photo={owner?.avatar}
+        title={owner?.name}
+        flexDir="row-reverse"
+      />
     </Box>
   );
 };
