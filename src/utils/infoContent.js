7d0be8c77d065706c5acc71c1bac4a2320ec0570
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-12 12:19:02 +0530

    Update infoContent.js

--- src/utils/infoContent.js
+++ src/utils/infoContent.js
@@ -2,9 +2,18 @@
 // This is particularly for the Help&Info page
 
 export const companyInfo = {
-  content:
-    `Evoia is a Property management platform where Properties can be uploaded 
+  content: `Evoia is a Property management platform where Properties can be uploaded 
     by agents and then could be used for selling or advertising the properties. 
     Real-Estate agents can login to the app with their Google account and quickly create their profile
     and start uploading properties. A step-by-step guide is also provided below.`,
+  aboutMe: `
+  I'm a skilled Frontend Developer with a passion for transforming business requirements 
+  into meaningful tech applications. With a deep understanding of both technology and business, 
+  I create user-friendly and visually appealing websites and web applications.
+   
+  You can find me on Twitter and GitHub, where I share my latest projects and insights. `,
+  vision: `Hi, I'm Sujoy Dutta, creator and admin of Evoia. I created this app with an intent 
+  to make life easier for Real-estate agents to create and update properties seamlessly with 
+  creating a massive data store of all types of properties that will drive their revenue high. 
+  `,
 };
