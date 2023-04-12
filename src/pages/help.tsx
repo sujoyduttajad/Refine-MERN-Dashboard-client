0f18b2a13239c6200e1b7fe59403e7d4d8657eb1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-12 08:10:12 +0530

    Update help.tsx

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@pankod/refine-mui";
 
 const HelpAndInfo = () => {
   return (
-    <div>HelpAndInfo</div>
-  )
-}
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Help & Information Booth
+      </Typography>
+      <Box
+        mt={2.5}
+        maxWidth="65rem"
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#fafafa"
+      >
+        <Stack direction={{xs: "column", sm: "row"}} >
+            <Typography>What is Evoia</Typography>
+            <Typography></Typography>
+        </Stack>
+      </Box>
+    </Box>
+  );
+};
 
-export default HelpAndInfo
+export default HelpAndInfo;
