ff534174819a1ac6c786ef982462529f86e12b69
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-16 12:17:54 +0530

    Create Resources.tsx

---
+++ src/components/common/Resources.tsx
@@ -0,0 +1,17 @@
+import { Box, Typography } from "@pankod/refine-mui";
+
+const Resources = () => {
+  return (
+    <Box
+      mt={2.5}
+      maxWidth="65rem"
+      borderRadius="15px"
+      padding="20px"
+      bgcolor="#fafafa"
+    >
+      <Typography></Typography>
+    </Box>
+  );
+};
+
+export default Resources;
