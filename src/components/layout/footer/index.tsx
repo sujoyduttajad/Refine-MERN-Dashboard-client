71f4ae1c501c0fc3d570e43aa1c08ff674610333
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-16 19:25:19 +0530

    Update index.tsx

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -7,32 +7,11 @@ import {
   Avatar,
   IconButton,
   Box,
+  height,
 } from "@pankod/refine-mui";
 
 import { ColorModeContext } from "contexts";
 import { NotificationsOutlined } from "@mui/icons-material";
-import { makeStyles } from "@pankod/refine-mui";
-
-const useStyles = makeStyles(() => ({
-  footer: {
-    backgroundColor: "#232323",
-    color: "#ffffff",
-    padding: 4,
-    textAlign: "center",
-  },
-  footerText: {
-    fontSize: "1rem",
-    fontWeight: 400,
-    margin: 1,
-  },
-  footerLink: {
-    color: "#ffffff",
-    textDecoration: "none",
-    "&:hover": {
-      textDecoration: "underline",
-    },
-  },
-}));
 
 export const Footer: React.FC = () => {
   const shouldRenderFooter = true; // since we are using the dark/light toggle; we don't need to check if user is logged in or not.
@@ -44,18 +23,41 @@ export const Footer: React.FC = () => {
         color: "#ffffff",
         padding: 4,
         textAlign: "center",
+        minHeight: "20vh",
+        height: "fit-content",
       }}
     >
       <Box>
-        Made with ❤️ by{" "}
-        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-          Your Name
-        </a>
-      </Box>
-      <Box>
-        <Typography>
-          © {new Date().getFullYear()} All rights reserved
-        </Typography>
+        <Box>
+          <Typography sx={{ color: "#fafafa" }}>
+            Made with ❤️ by Sujoy
+          </Typography>
+          <Typography>
+            Find me in{" "}
+            <a
+              href="https://github.com/sujoyduttajad"
+              rel="noopener noreferrer"
+              style={{ color: "#fafafa" }}
+            >
+              GitHub
+            </a>
+          </Typography>
+          <Typography>
+            Find me in{" "}
+            <a
+              href="https://twitter.com/SujoyDutta4290"
+              rel="noopener noreferrer"
+              style={{ color: "#fafafa" }}
+            >
+              Twitter
+            </a>
+          </Typography>
+        </Box>
+        <Box>
+          <Typography>
+            © {new Date().getFullYear()} All rights reserved
+          </Typography>
+        </Box>
       </Box>
     </footer>
   ) : null;
