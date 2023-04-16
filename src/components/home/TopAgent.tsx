cadea0166a8e4fc0bb8788c96a2bba5b29f69bd7
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-16 19:15:42 +0530

    Update TopAgent.tsx

--- src/components/home/TopAgent.tsx
+++ src/components/home/TopAgent.tsx
@@ -3,26 +3,32 @@ import { Box, CardMedia, Stack, Typography } from "@pankod/refine-mui";
 import { useList } from "@pankod/refine-core";
 import { Error, Loading } from "components/common/Loading&Error";
 import ThreeDotsMenu from "components/common/ThreeDotsMenu";
+// import { useNavigate } from "@pankod/refine-react-router-v6";
 
 const TopAgent = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const handleNavigate = () => {};
 
   const { data, isLoading, isError } = useList({
     resource: "users",
   });
 
-  const topAgents = data?.data.sort((a, b) => b.__v - a.__v).slice(0, 4) ?? [];
-
   if (isLoading) return <Loading />;
   if (isError) return <Error />;
+
+  const topAgents = data?.data.sort((a, b) => b.__v - a.__v).slice(0, 4) ?? [];
+
+  // const navigate = useNavigate();
+  const handleNavigate = () => {
+    // navigate("/properties/create");
+  };
 
   return (
     <Box>
