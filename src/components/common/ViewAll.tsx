251655d2ec5776beef9cb2bc62a152397cd3baac
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-05-14 19:17:59 +0530

    Update ViewAll.tsx

--- src/components/common/ViewAll.tsx
+++ src/components/common/ViewAll.tsx
@@ -1,25 +1,28 @@
 import { Button } from "@pankod/refine-mui";
-import React from "react";
+import { Link } from "@pankod/refine-react-router-v6";
+import { ViewAllProps } from "interfaces/home";
 
-const ViewAll = () => {
+const ViewAll = ({ pathName} : ViewAllProps) => {
   return (
-    <Button
-      variant="outlined"
-      size="small"
-      type="button"
-      sx={{
-        textTransform: "capitalize",
-        color: "#808191",
-        borderColor: "#E4E4E4",
-        "&:hover": {
+    <Link to={pathName}>
+      <Button
+        variant="outlined"
+        size="small"
+        type="button"
+        sx={{
+          textTransform: "capitalize",
+          color: "#808191",
+          borderColor: "#E4E4E4",
+          "&:hover": {
             borderColor: "#475BE8",
             color: "#475BE8",
-            backgroundColor: "#F2F3FD"
-        }
-      }}
-    >
-      View All
-    </Button>
+            backgroundColor: "#F2F3FD",
+          },
+        }}
+      >
+        View All
+      </Button>
+    </Link>
   );
 };
 
