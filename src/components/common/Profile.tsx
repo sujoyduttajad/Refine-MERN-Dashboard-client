1f033091388a61c746921fdc8ac5bd1eda55a637
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-05-29 01:32:40 +0530

    Update Profile.tsx

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -172,8 +172,8 @@ const Profile = ({ type, name, avatar, email, properties }: ProfileProps) => (
 
     {properties.length > 0 && (
       <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#FCFCFC">
-        <Typography fontSize={18} fontWeight={600} color="#11142D">
-          {type} Properties
+        <Typography fontSize={20} fontWeight={700} color="#11142D">
+          {type} Properties ({properties?.length})
         </Typography>
 
         <Box
