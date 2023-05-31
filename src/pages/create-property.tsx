1589156898650c60c1f29a00497e676f31b5c3f7
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-05-31 17:17:22 +0530

    Update create-property.tsx

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useGetIdentity } from "@pankod/refine-core";
 import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
-import { useNavigate } from "@pankod/refine-react-router-v6";
+// import { useNavigate } from "@pankod/refine-react-router-v6";
 import Form from "components/common/Form";
 
 const CreateProperty = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const { data: user } = useGetIdentity();
 
   const [propertyImage, setPropertyImage] = useState({ name: "", url: "" });
