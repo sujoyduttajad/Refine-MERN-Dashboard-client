2c7a8714f34d4e8c5268a3dc1b4354bbf917e866
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-05-31 19:14:18 +0530

    Update CountProperties.tsx

--- src/components/charts/CountProperties.tsx
+++ src/components/charts/CountProperties.tsx
@@ -1,14 +1,19 @@
-import { Stack } from '@pankod/refine-mui';
-import { Box, Typography } from '@pankod/refine-mui';
-import ReactApexChart from 'react-apexcharts';
-import { PropertiesAreaSeries, PropertiesPerMonth } from './chart.config';
+import { Box, Typography } from "@pankod/refine-mui";
+import ReactApexChart from "react-apexcharts";
+import { PropertiesPerMonth } from "./chart.config";
 
-interface TotalPrice {
-    totalPrice: number;
-  }
+interface TotalProperties {
+  monthNames: number[];
+}
 
+const CountProperties = ({ monthNames }: TotalProperties) => {
+  const PropertiesAreaSeries = [
+    {
+      name: "Properties",
+      data: monthNames,
+    },
+  ];
 
-const CountProperties = () => {
   return (
     <Box
       p={4}
@@ -22,16 +27,15 @@ const CountProperties = () => {
       <Typography fontSize={18} fontWeight={600} color="#11142d">
         Properties per month
       </Typography>
-     
 
       <ReactApexChart
         series={PropertiesAreaSeries}
-        type="area"
+        type="line"
         height={310}
         options={PropertiesPerMonth}
       />
     </Box>
-  )
-}
+  );
+};
 
-export default CountProperties
+export default CountProperties;
