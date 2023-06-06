fc0609e177c132275e2fae749d75f2515f2fbf67
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-06-07 02:45:36 +0530

    Update help.tsx

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -54,6 +54,7 @@ const HelpAndInfo = () => {
         borderRadius="15px"
         padding="20px"
         bgcolor="#fafafa"
+        color="#333"
       >
         <Typography variant="h5" fontWeight={600}>
           About the Creator
