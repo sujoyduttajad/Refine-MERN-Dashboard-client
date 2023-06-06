1fd1bbb805f981d6317e270bc646ad5949ac11c9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-06-07 02:45:39 +0530

    Update Resources.tsx

--- src/components/common/Resources.tsx
+++ src/components/common/Resources.tsx
@@ -9,6 +9,7 @@ const Resources = () => {
       borderRadius="15px"
       padding="20px"
       bgcolor="#fafafa"
+      color="#333"
     >
       <Typography variant="h5" fontWeight={600} mb={3}>
         Property Links
